0277da266a415decbae0e51d3f783f0f9d70c080
dpickel <dpickel@cisco.com>
2026-01-12 13:43:16 +0200

    Add Tandoor API configuration to support recipe management

--- config.py
+++ config.py
@@ -13,7 +13,9 @@ class Config:
     RECIPE_LANG: str = os.getenv("RECIPE_LANG", "hebrew")
     MEALIE_API_KEY: str = os.getenv("MEALIE_API_KEY", "")
     MEALIE_HOST: str = os.getenv("MEALIE_HOST", "http://192.168.127.251:9925")
+    TANDOOR_API_KEY: str = os.getenv("TANDOOR_API_KEY", "")
+    TANDOOR_HOST: str = os.getenv("TANDOOR_HOST", "https://tandoor.pickel.me")
     TARGET_LANGUAGE: str = os.getenv("TARGET_LANGUAGE", "he")
-
+    OUTPUT_TARGET: str = os.getenv("OUTPUT_TARGET", "tandoor")
 
 config = Config()
